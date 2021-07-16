1bf84a4292ada72f08045f1c57bc63db695a5d8a
Giovanny Fialho <giovannyfialho@gfsilvas-Mac-mini.local>
2021-07-16 15:17:55 -0300

    criacao de menu fullwidth e criacao de menu sider bar

--- src/static/js/menu.js
+++ src/static/js/menu.js
@@ -1,13 +1,21 @@
-document.getElementById("menu-hamburguer").addEventListener("click", (event) => {
-    if (event.target.checked == true) {
-        document.querySelector(".menu-container-items").classList.add("open");
-        event.target.checked = false;
-    }
-});
+if (document.getElementById("menu-hamburguer")) {
+    document.getElementById("menu-hamburguer").addEventListener("click", (event) => {
+        if (event.target.checked == true) {
+            document.querySelector(".menu-container-items").classList.add("open");
+            event.target.checked = false;
 
-document.getElementById("close-menu-hamburguer").addEventListener("click", (event) => {
-    if (event.target.checked == false) {
-        document.querySelector(".menu-container-items").classList.remove("open");
-        event.target.checked = true;
-    }
-});
+            document.body.classList.add("menuIsOpen");
+        } else {
+            document.body.classList.remove("menuIsOpen");
+        }
+    });
+}
+
+if (document.getElementById("close-menu-hamburguer")) {
+    document.getElementById("close-menu-hamburguer").addEventListener("click", (event) => {
+        if (event.target.checked == false) {
+            document.querySelector(".menu-container-items").classList.remove("open");
+            event.target.checked = true;
+        }
+    });
+}
